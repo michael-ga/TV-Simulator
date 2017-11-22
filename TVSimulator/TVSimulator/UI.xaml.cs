5d2adc1b0b28949e6505bf7bd574fd4116fb5777
michaelga <xanmichael@gmail.com>
2017-11-22 09:56:41 +0200

    temp fullscreen

--- TVSimulator/TVSimulator/UI.xaml.cs
+++ TVSimulator/TVSimulator/UI.xaml.cs
@@ -15,76 +15,20 @@ namespace TVSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool isSubfolders = false;
+        public bool isSubfolders = false, IsFullscreen=false;
         TimeSpan t = new TimeSpan(0, 2, 3);
+        private Size _previousVideoContainerSize = new Size();
+
         public MainWindow()
         {
+            //this.WindowState = WindowState.Maximized;
+
             InitializeComponent();
-
+           
         }
 
+    
 
-        private void chooseFolderBtn_Click(object sender, RoutedEventArgs e)
-        {
-            using (var folderDialog = new FolderBrowserDialog())
-            {
-                if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    FileImporter fm = new FileImporter();
-                    fm.OnVideoLoaded += onVideoRecievedHandler;
-                    fm.getAllMediaFromDirectory(folderDialog.SelectedPath, isSubfolders);
-                    fm.getAllMediaFromDirectory(folderDialog.SelectedPath,true);
-                }
-            }
-        }
-        // event handler raised when data of enterred pathes is loaded on fileImporter.
-        private void onVideoRecievedHandler(Object o,List<Video> arg)
-        {
-            System.Windows.MessageBox.Show("Done!!!");
-            Random r = new Random();
-            int x = r.Next(arg.Count);
-
-            playVideoFromPosition(arg.ElementAt(x).Path, t); ;
-        }
-
-        private void selectFileBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = ("MKV|*.mkv");
-            ofd.RestoreDirectory = true;
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                folderPathTextbox.Text = ofd.FileName;
-            }
-        }
-
-        private void playVideoFromPosition(string path, TimeSpan t)
-        {
-            mediaPlayer.Source = new Uri(path);
-            mediaPlayer.Position = t;
-            mediaPlayer.Play();
-        }
-
-        #region Media player functions
-
-        private void stopBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!(Directory.Exists(folderPathTextbox.Text)))
-                return;
-
-
-        }
-
-
-
-        private void playBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var f = new FileImporter();
-            f.createDB();
-
-            //mediaPlayer.Source = new Uri(folderPathTextbox.Text);
-            //mediaPlayer.Play();
-        }
 
         // Play the media.
         void OnMouseDownPlayMedia(object sender, MouseButtonEventArgs args)
@@ -106,7 +50,104 @@ namespace TVSimulator
 
         }
 
+
+        #region button listeners
+        private void chooseFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    FileImporter fm = new FileImporter();
+                    fm.OnVideoLoaded += onVideoRecievedHandler;
+                    fm.getAllMediaFromDirectory(folderDialog.SelectedPath, isSubfolders);
+                   
+                }
+            }
+        }
+
+        private void selectFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = ("MKV|*.mkv");
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderPathTextbox.Text = ofd.FileName;
+                play(ofd.FileName);
+            }
+        }
+
+
+
+        private void stopBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Source = new Uri(folderPathTextbox.Text);
+            mediaPlayer.Play();
+            FullscreenToggle();
+
+        }
+
+
+        #endregion button listeners
+
+
+
+
+        #region Media player functions
+
+        private void FullscreenToggle()
+        {
+            this.IsFullscreen = !this.IsFullscreen;
+            if (this.IsFullscreen)
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+                mediaPlayer.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+                mediaPlayer.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            }
+            else
+            {
+                mediaPlayer.Width = _previousVideoContainerSize.Width;
+                mediaPlayer.Height = _previousVideoContainerSize.Height;
+            }
+        }
+
+
+
+        private void playVideoFromPosition(string path, TimeSpan t)
+        {
+            mediaPlayer.Position = t;
+            play(path);
+        }
+
+        private void play(string path)
+        {
+            mediaPlayer.Source = new Uri(path);
+            mediaPlayer.Play();
+        }
+
         #endregion Media player functions
+
+
+
+        // event handler raised when data of enterred pathes is loaded on fileImporter.
+        private void onVideoRecievedHandler(Object o, List<Video> arg)
+        {
+            System.Windows.MessageBox.Show("Done!!!");
+            Random r = new Random();
+            int x = r.Next(arg.Count);
+
+            playVideoFromPosition(arg.ElementAt(x).Path, t); ;
+        }
 
     }
 }
