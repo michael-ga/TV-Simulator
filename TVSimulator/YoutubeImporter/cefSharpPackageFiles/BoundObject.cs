80d8073abd9cb85918469f763dfe6530880e3c0a
michaelga <xanmichael@gmail.com>
2017-12-20 20:20:17 +0200

    install and packets of cfe

---
+++ TVSimulator/YoutubeImporter/cefSharpPackageFiles/BoundObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeImporter.Cef
+{
+    class BoundObject
+    {
+        public event EventHandler PlayerLoadingDone;
+        public event EventHandler PlayerQualityChanged;
+        public event EventHandler<YoutubePlayerState> PlayerPlayingChanged;
+
+        public void PlayerLoaded()
+        {
+            if(PlayerLoadingDone != null)
+            {
+                PlayerLoadingDone(this, new EventArgs());
+            }
+        }
+
+        public void qualityChanged()
+        {
+            PlayerQualityChanged?.Invoke(this, new EventArgs());
+        }
+
+        public void PlayingChanged(int state)
+        {
+            PlayerPlayingChanged?.Invoke(this, state.ParseToYoutubeState());
+        }
+    }
+}
