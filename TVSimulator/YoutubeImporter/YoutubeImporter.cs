daa67f329c906977efdeb9ce1c68c8bf7ee4f6d8
michaelga <xanmichael@gmail.com>
2017-12-20 14:02:39 +0200

    fix startup

--- TVSimulator/YoutubeImporter/YoutubeImporter.cs
+++ TVSimulator/YoutubeImporter/YoutubeImporter.cs
@@ -1,19 +1,11 @@
-﻿
-
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Upload;
+﻿using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using MediaClasses;
 using MyToolkit.Multimedia;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
-
 namespace YoutubeImporter
 {
     public class Search
