debc1372cde7d36a2dd67ba26d5c3341f429bb45
royok17 <royok17@outlook.com>
2017-12-24 13:55:53 +0200

    added channel and mediaSchedule classes

---
+++ TVSimulator/MediaClasses/Channel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaClasses
+{
+    class Channel
+    {
+        private string name;     //the name need to be channel main genre 
+        private MediaSchedule schedule;
+        private Boolean type;    //local or youtube streaming TODO:local variables to LOCAL_CHANNEL , YOUTUBE_CHANNEL
+        private Media playNow;
+        private Media playNext;  // optional
+
+        public Channel()
+        { }
+
+        public Channel(string name,MediaSchedule schedule,Boolean type)
+        {
+            this.name = name;
+            this.schedule = schedule;
+            this.type = type;
+            //TODO:playNow and playNext is done when the schedule will be ready
+        }
+
+        //TODO:play function - play the current media from the schedule here in the constructor
+
+    }
+}
---
+++ TVSimulator/MediaClasses/MediaSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaClasses
+{
+    class MediaSchedule
+    {
+        private List<TimeSpan> durationList;
+        private List<string> sourcesList;
+        private int numOfMedia;
+        private TimeSpan totalDuration;
+
+        public MediaSchedule() {   }
+    
+        //TODO: constructor and functions
+    }
+
+}
