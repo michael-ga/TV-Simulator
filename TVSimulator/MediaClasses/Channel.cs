8388f7d8ebbe76454a1a2f977ed63c1301658de7
royok17 <royok17@outlook.com>
2017-12-26 10:20:36 +0200

    edit channel class

--- TVSimulator/MediaClasses/Channel.cs
+++ TVSimulator/MediaClasses/Channel.cs
@@ -6,21 +6,22 @@ namespace MediaClasses
 {
     class Channel
     {
-        private string name;     //the name should be channel main genre 
+        private int channelNumber;
+        private string typeOfMedia;       //movies,tv serias,music or youtube stream
+        private string genre;             //the name should be channel main genre 
         private MediaSchedule schedule;
-        private Boolean type;    //local or youtube streaming 
         private Media playNow;   //which media played right now
         private Media playNext;  // optional
-        private Boolean isWatchingNow;    // if user watching the channel- 1 else- 0
         
         public Channel()
         { }
 
-        public Channel(string name,MediaSchedule schedule,Boolean type)
+        public Channel(int channelNumber,string typeOfMedia,string genre,MediaSchedule schedule)
         {
-            this.name = name;
+            this.channelNumber = channelNumber;
+            this.genre = genre;
+            this.typeOfMedia = typeOfMedia;
             this.schedule = schedule;
-            this.type = type;
             //TODO:playNow and playNext is done when the schedule will be ready
         }
 
